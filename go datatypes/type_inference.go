d26d235e7f76c68e3235ee1f176457d0fd2d801c
Pavan Kumar Uppuluri <63893920+pavanuppuluri@users.noreply.github.com>
2022-05-16 21:58:04 +0530

    Data type inference in Go

---
+++ go datatypes/type_inference.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+   x := 1
+   y := 2.0
+   z := "I am a string"   
+   
+   fmt.Printf("x is of type %T\n", x)
+   fmt.Printf("y is of type %T\n", y)	
+   fmt.Printf("z is of type %T\n", z)	
+}
