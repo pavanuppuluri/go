65290bbb99e187d5e2c0f7cf4a9b59e15b2faf66
Pavan Kumar Uppuluri <63893920+pavanuppuluri@users.noreply.github.com>
2022-05-19 17:45:22 +0530

    Search a word in a given string

---
+++ go programs/contains.go
@@ -0,0 +1,15 @@
+
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+
+	// using the function
+	fmt.Println(strings.Contains("Hi I am Pavan. Welcome to my github", "Pavan"))
+	fmt.Println(strings.Contains("hellow world", "ok"))
+
+}
